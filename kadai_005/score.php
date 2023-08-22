3d7aaa29971e821c563ae139a57450e34ab736aa
yui-kuta <137240438+yui-kuta@users.noreply.github.com>
2023-08-22 23:00:10 +0900

    added kadai_005

---
+++ kadai_005/score.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHPの基礎を学ぼう_第5章課題</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+        //  変数にテストの合計点を代入
+         $score = 80+60+55+40+100+25+80+95+30+60;
+         echo $score/10;
+         ?>
+     </p>
+ </body>
+ 
+ </html>
